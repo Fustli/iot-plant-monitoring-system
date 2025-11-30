5b10803503e8321f863dedd4caa589012d406db2
Hasznos Norbert Bendegúz <norben2002@gmail.com>
2025-11-30 18:56:24 +0100

    Fix: CardTheme -> CardThemeData for Flutter Material 3

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
                 foregroundColor: AppColors.neutral0,
                 elevation: 0,
               ),
-              cardTheme: CardTheme(
+              cardTheme: CardThemeData(
                 elevation: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
